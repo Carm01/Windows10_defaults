abda01d184e2a5c63cf61da687ceac8b47f2c9c8
Carm01 <carmine900@gmail.com>
2019-03-10 01:23:38 -0500

    Update ActivateAdmin.cmd

--- ActivateAdmin.cmd
+++ ActivateAdmin.cmd
@@ -1,6 +1,6 @@
 net user administrator /active:yes
 net user  Administrator apple2pie
 wmic USERACCOUNT WHERE Name='administrator' SET PasswordExpires=FALSE
-net user localadmin L0c4ladmin /add /passwordchg:NO
+net user localadmin password1 /add /passwordchg:NO
 net localgroup "Administrators" "localadmin" /add
 netsh interface ip set dns "local area connection" dhcp
